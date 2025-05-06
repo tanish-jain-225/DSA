84c586ef2cef5ab28b123e35ef758ecedd47e432
Tanish Jain <142605718+tanish-jain-225@users.noreply.github.com>
2025-05-06 17:09:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (69.09%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverseNumber(int ognum) {
+        int reversedNum = 0; // Initialize reversed number
+        while (ognum != 0) {
+            int digit = ognum % 10; // Get the last digit
+            if(reversedNum>INT_MAX/10 || reversedNum<INT_MIN/10)
+            {
+                return 0;
+            }
+            reversedNum = reversedNum * 10 + digit; // Update the reversed number
+            ognum /= 10; // Remove the last digit from the original number
+        }
+        return reversedNum; // Return the reversed number
+    }
+    bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        int revNum = reverseNumber(x);
+        return (x == revNum);
+    }
+};
