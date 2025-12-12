2bb202ce9f9cfac9c26976d64acb7ac8bd813044
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2025-12-12 22:21:01 +0530

    Time: 52 ms (40.21%) | Memory: 28.9 MB (98.98%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue; // Skip duplicates for i
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[j], nums[k]});
+
+                    // Skip duplicates for j and k
+                    while (j < k && nums[j] == nums[j + 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k - 1])
+                        k--;
+
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
+        return result;
+    }
+};
