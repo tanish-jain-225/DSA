f3f46cae3df96475929d00b602b2ac3410a3eab5
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-02-21 19:01:12 +0530

    Time: 2 ms (31.46%) | Memory: 29 MB (70.54%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+
+        // Base case: All in one
+        if (p == nullptr || q == nullptr) {
+            return p == q; // Both should be null to be the same
+        }
+
+        int isLeftSame =
+            isSameTree(p->left, q->left); // Check if left subtrees are the same
+        int isRightSame = isSameTree(
+            p->right, q->right); // Check if right subtrees are the same
+
+        // Both left and right subtrees must be the same, and the current node
+        // values must be the same
+        return isLeftSame && isRightSame && (p->val == q->val);
+    } // TC: O(n) where n is the number of nodes in the smaller tree, SC: O(h)
+      // where h is the height of the tree (due to recursion stack)
+
+    // Find if a tree is a subtree of another tree
+    bool
+    isSubtree(TreeNode* s,
+              TreeNode* t) // TC: O(N*M) where N is the number of nodes in tree
+                           // s and M is the number of nodes in tree t)
+    {
+        if (s == NULL || t == NULL) // Base case: All in one
+        {
+            return s == t; // Both should be null to be the same
+        }
+
+        if (s->val == t->val &&
+            isSameTree(s, t)) // Check if the current nodes match and if the
+                              // trees are the same
+        {
+            return true; // Found a match, check if the trees are the same
+        }
+
+        return isSubtree(s->left, t) ||
+               isSubtree(s->right,
+                         t); // Recursively check left and right subtrees
+    }
+};
