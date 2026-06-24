e80fd10a0378a01ec2ece02f8ab9d0a88c138974
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-06-24 22:15:19 +0530

    Time: 39 ms (94.96%) | Memory: 28.9 MB (92.36%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> result;
+
         sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> answer;
 
-        for (int i = 0; i < n - 2; i++) {
+        for (int i = 0; i < n; i++) {
             if (i > 0 && nums[i] == nums[i - 1])
-                continue; // Skip duplicates for i
+                continue;
 
-            int j = i + 1;
-            int k = n - 1;
+            if (nums[i] > 0)
+                break;
 
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+            int target = nums[i];
+            int st = i + 1;
+            int end = n - 1;
 
-                if (sum == 0) {
-                    result.push_back({nums[i], nums[j], nums[k]});
+            while (st < end) {
+                if (nums[st] + nums[end] == -target) {
+                    answer.push_back({target, nums[st], nums[end]});
+                    st++;
+                    end--;
 
-                    // Skip duplicates for j and k
-                    while (j < k && nums[j] == nums[j + 1])
-                        j++;
-                    while (j < k && nums[k] == nums[k - 1])
-                        k--;
+                    while (st < end && nums[st] == nums[st - 1])
+                        st++;
 
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    j++;
+                    while (st < end && nums[end] == nums[end + 1])
+                        end--;
+
+                    continue;
+                }
+                if (nums[st] + nums[end] > -target) {
+                    end--;
                 } else {
-                    k--;
+                    st++;
                 }
             }
         }
-
-        return result;
+        return answer;
     }
 };
