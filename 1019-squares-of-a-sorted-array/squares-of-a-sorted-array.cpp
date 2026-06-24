80c2d67808c47aa803484b1e1944bff22424ccb7
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-06-24 20:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (39.91%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,75 +1,27 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+
         int n = nums.size();
-        vector<int> pos;
-        vector<int> neg;
-        vector<int> answer;
+        vector<int> answer(n);
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] >= 0) {
-                pos.push_back(nums[i]);
+        int left = 0;
+        int right = n - 1;
+        int idx = n - 1;
+
+        while (left <= right) {
+
+            if (abs(nums[left]) > abs(nums[right])) {
+                answer[idx] = nums[left] * nums[left];
+                left++;
             } else {
-                neg.push_back(nums[i]);
+                answer[idx] = nums[right] * nums[right];
+                right--;
             }
+
+            idx--;
         }
 
-        int ps = pos.size();
-        int ns = neg.size();
-
-        if (ps == 0) // negative Only
-        {
-            reverse(neg.begin(), neg.end()); // reverse
-            for (int i = 0; i < ns; i++) {
-                neg[i] = neg[i] * neg[i]; // Squared
-                answer.push_back(neg[i]);
-            }
-            return answer;
-        }
-
-        if (ns == 0) // positive only
-        {
-            for (int i = 0; i < ps; i++) {
-                pos[i] = pos[i] * pos[i]; // Squared
-                answer.push_back(pos[i]);
-            }
-            return answer;
-        }
-
-        // square pos
-        for (int i = 0; i < ps; i++) {
-            pos[i] = pos[i] * pos[i]; // General Squared
-        }
-
-        // reverse neg
-        reverse(neg.begin(), neg.end());
-        // square neg
-        for (int i = 0; i < ns; i++) {
-            neg[i] = neg[i] * neg[i]; // General Squared
-        }
-
-        int first = 0;
-        int second = 0;
-
-        while (first < ps && second < ns) {
-            if (pos[first] >= neg[second]) {
-                answer.push_back(neg[second]);
-                second++;
-            } else {
-                answer.push_back(pos[first]);
-                first++;
-            }
-        }
-
-        while (first < ps) {
-            answer.push_back(pos[first]);
-            first++;
-        }
-
-        while (second < ns) {
-            answer.push_back(neg[second]);
-            second++;
-        }
         return answer;
     }
 };
