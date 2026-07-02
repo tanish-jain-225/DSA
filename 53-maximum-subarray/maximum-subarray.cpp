d6c96f63a3f3f9167b7567d458ab9c7e0c113789
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-07-02 19:58:45 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (80.79%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int currSum = 0; 
-        int maxSum = INT_MIN;
+        int n = nums.size();      // Size/Range
+        int bestEnding = nums[0]; // Case Selector
+        int ans = nums[0];        // Initial Assumed Answer
 
-        for(int val: nums){
-            currSum += val;
-            maxSum = max(currSum, maxSum);
-            if(currSum < 0)
-            {
-                currSum = 0;
-            }
-        }   
+        for (int i = 1; i < n; i++) // From 1 to n
+        {
+            int v1 = bestEnding + nums[i]; // Case 1
+            int v2 = nums[i];              // Case 2
+            bestEnding = max(v1, v2);   // Selected Best Case of Current Index
+            ans = max(bestEnding, ans); // Max of best case and previous answer
+        }
 
-        return maxSum; // Time Complexity is O(n)
+        return ans; // Final Answer
     }
 };
