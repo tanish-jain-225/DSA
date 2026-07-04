4325bbcaa022d1c8cb463e68cb204f840b9941c3
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-07-04 22:19:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (59.62%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -5,18 +5,21 @@ public:
 
         int best = nums[0];
         int worst = nums[0];
-
         int answer = nums[0];
 
         for (int i = 1; i < n; i++) {
+
+            int prevBest = best;
+            int prevWorst = worst;
+
             int v1 = nums[i];
-            int v2 = best * nums[i];
-            int v3 = worst * nums[i];
+            int v2 = prevBest * nums[i];
+            int v3 = prevWorst * nums[i];
 
             best = max(v1, max(v2, v3));
             worst = min(v1, min(v2, v3));
 
-            answer = max(answer, max(best, worst));
+            answer = max(answer, best);
         }
 
         return answer;
