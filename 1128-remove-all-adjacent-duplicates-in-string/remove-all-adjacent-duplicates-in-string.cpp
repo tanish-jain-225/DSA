c7b94a975263086ca0760b71cd8052a977d89253
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-07-08 18:28:09 +0530

    Time: 14 ms (17.97%) | Memory: 14.4 MB (32.08%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int n = s.size();
+        stack<char> st;
+        string res;
+
+        for (int i = 0; i < n; i++) // Fill stack
+        {
+            if (st.empty()) // First iteration
+            {
+                st.push(s[i]);
+                continue;
+            }
+
+            if (st.top() == s[i]) // pop condition
+            {
+                st.pop();
+                continue;
+            }
+
+            st.push(s[i]); // default stack pushes
+        }
+
+        while (!st.empty()) // empty stack till zeroed
+        {
+            res.push_back(st.top()); // store answer
+            st.pop();       // remove current item
+        }
+
+        // Since answer is reversed, to get back to og state
+        reverse(res.begin(), res.end());
+
+        return res;
+    }
+};
