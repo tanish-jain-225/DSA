df6c5bbccfbfc2418e05f6f560eb0fe30870f9d1
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-07-08 21:34:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (67.79%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
-    bool isValid(string s) { // using stack
+    bool isValid(string s) {
 
-        // stack to keep track of opening brackets
+        int n = s.size();
+
         stack<char> st;
 
-        // length of the string
-        int n = s.length();
-
-        // iterate through each character in the string
         for (int i = 0; i < n; i++) {
-            char ch = s[i];
-            if (ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch); // push opening brackets onto stack
-            } else {
-                // if stack is empty when we encounter a closing bracket, return
-                // false - no matching opening bracket
+
+            // Opening brackets
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            }
+
+            // Closing brackets
+            else {
+
+                // No opening bracket available
                 if (st.empty())
                     return false;
 
-                if ((ch == ')' && st.top() == '(') ||
-                    (ch == '}' && st.top() == '{') ||
-                    (ch == ']' && st.top() == '[')) // ch matches with st.top()
-                {
-                    st.pop(); // pop the matching opening bracket from stack
-                } else {
-                    return false; // brackets do not match
+                char open = st.top();
+
+                // Matching pair
+                if ((open == '(' && s[i] == ')') ||
+                    (open == '{' && s[i] == '}') ||
+                    (open == '[' && s[i] == ']')) {
+
+                    st.pop();
+                }
+                else {
+                    return false;
                 }
             }
         }
-        return st.empty(); // if stack is empty, all brackets matched;
-                           // otherwise, return false
+
+        // Stack should be empty
+        return st.empty();
     }
 };
