6db2d784750d0df91d9b5c212efa0657d352e54f
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-07-10 20:04:16 +0530

    Time: 2 ms (59.46%) | Memory: 12.1 MB (11.62%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -4,19 +4,25 @@ public:
         unordered_map<char, int> need;
         unordered_map<char, int> have;
 
-        for (char c : ransomNote)
-            need[c]++;
+        // Count frequency of characters needed
+        for (int i = 0; i < ransomNote.size(); i++) {
+            need[ransomNote[i]]++;
+        }
 
-        for (char c : magazine)
-            have[c]++;
+        // Count frequency of characters available
+        for (int i = 0; i < magazine.size(); i++) {
+            have[magazine[i]]++;
+        }
 
+        // Compare frequencies
         for (auto i : need) {
             char c = i.first;
             int fneed = i.second;
             int fhave = have[c];
 
-            if (fhave < fneed)
+            if (fhave < fneed) {
                 return false;
+            }
         }
 
         return true;
