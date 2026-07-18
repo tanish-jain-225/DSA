1f7a963ee6f9bc4f9a5e3bdf4efd465bfedfd30e
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-07-18 20:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (35.05%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,54 +1,45 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-
         int n = nums.size();
-
-        // ---------------- Part 1 : Find Pivot ----------------
-
         int low = 0;
         int high = n - 1;
-        int idx = 0; // Index of minimum element (pivot)
 
         while (low <= high) {
-
             int guess = (low + high) / 2;
 
-            if (nums[guess] > nums[n - 1]) {
-                low = guess + 1;
-            } else {
-                idx = guess;
-                high = guess - 1;
+            if (nums[guess] > nums[n - 1]) // Part 1
+            {
+                if (nums[guess] == target) {
+                    return guess;
+                }
+
+                if (nums[guess] < target) {
+                    low = guess + 1;
+                } else {
+                    if (nums[0] > target) {
+                        low = guess + 1;
+                    } else {
+                        high = guess - 1;
+                    }
+                }
+            } else // Part 2
+            {
+                if (nums[guess] == target) {
+                    return guess;
+                }
+
+                if (nums[guess] > target) {
+                    high = guess - 1;
+                } else {
+                    if (nums[n - 1] < target) {
+                        high = guess - 1;
+                    } else {
+                        low = guess + 1;
+                    }
+                }
             }
         }
-
-        // ---------------- Part 2 : Decide Search Region ----------------
-
-        if (target <= nums[n - 1]) {
-            low = idx;
-            high = n - 1;
-        } else {
-            low = 0;
-            high = idx - 1;
-        }
-
-        // ---------------- Part 3 : Normal Binary Search ----------------
-
-        while (low <= high) {
-
-            int guess = (low + high) / 2;
-
-            if (nums[guess] == target) {
-                return guess;
-            }
-
-            if (nums[guess] < target) {
-                low = guess + 1;
-            } else {
-                high = guess - 1;
-            }
-        }
-
         return -1;
     }
 };
