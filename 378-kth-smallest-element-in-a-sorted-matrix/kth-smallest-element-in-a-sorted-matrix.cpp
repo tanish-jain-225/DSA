e2b913d780180383627302b9439f002e442d9e06
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-07-20 22:15:09 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (65.57%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& a, int n, int m, int guess) {
-        int row = n - 1;
+    // Counts how many elements are <= guess
+    int helper(vector<vector<int>>& matrix, int n, int m, int guess) {
+
+        int row = n - 1; // Start from bottom-left
         int col = 0;
+
         int count = 0;
 
         while (row >= 0 && col < m) {
-            if (a[row][col] <= guess) {
-                count = count + row + 1;
+
+            // Current element is <= guess
+            if (matrix[row][col] <= guess) {
+
+                // All elements above in this column are also <= guess
+                count += row + 1;
+
+                // Move to next column
                 col++;
             } else {
+
+                // Current element is too large
+                // Move one row up
                 row--;
             }
         }
@@ -18,25 +30,38 @@ public:
     }
 
     int kthSmallest(vector<vector<int>>& matrix, int k) {
+
         int n = matrix.size();
         int m = matrix[0].size();
 
+        // Search Space
         int low = matrix[0][0];
         int high = matrix[n - 1][m - 1];
-        int res = -1;
 
+        int ans = -1;
+
+        // Binary Search on Answer
         while (low <= high) {
-            int guess = (low + high) / 2;
-            int ans = helper(matrix, n, m, guess);
 
-            if (ans < k) {
+            int guess = low + (high - low) / 2;
+
+            // Count elements <= guess
+            int count = helper(matrix, n, m, guess);
+
+            if (count < k) {
+
+                // Need larger values
                 low = guess + 1;
             } else {
-                res = guess;
+
+                // Possible answer
+                ans = guess;
+
+                // Try finding a smaller valid answer
                 high = guess - 1;
             }
         }
 
-        return res;
+        return ans;
     }
 };
