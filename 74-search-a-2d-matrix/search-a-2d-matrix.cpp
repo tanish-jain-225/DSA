be59540512a26a2d9bfb4649286d23bf55746036
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-07-20 19:47:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (45.26%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    bool searchInRow(const vector<vector<int>>& matrix, int target, int row) {
-        // Binary search in a specific row of a 2D matrix
-        int start = 0;
-        int end = matrix[0].size() - 1; // Number of columns in the matrix
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (matrix[row][mid] == target) {
-                return true;
-            } else if (matrix[row][mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return false;
-    }
-
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
 
-        int startRow = 0;
-        int endRow = m - 1;
-        while (startRow <= endRow) {
-            int midRow = startRow + (endRow - startRow) / 2;
-            if (target >= matrix[midRow][0] &&
-                target <= matrix[midRow][n - 1]) {
-                return searchInRow(matrix, target, midRow);
-            } else if (target < matrix[midRow][0]) {
-                endRow = midRow - 1;
-            } else {
-                startRow = midRow + 1;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        // Binary Search on the entire matrix
+        int low = 0;
+        int high = rows * cols - 1;
+
+        while (low <= high) {
+
+            // Middle index in the virtual 1D array
+            int guess = low + (high - low) / 2;
+
+            // Convert 1D index to 2D position
+            int r = guess / cols;
+            int c = guess % cols;
+
+            // Target found
+            if (matrix[r][c] == target) {
+                return true;
+            }
+
+            // Search in the right half
+            if (matrix[r][c] < target) {
+                low = guess + 1;
+            }
+            // Search in the left half
+            else {
+                high = guess - 1;
             }
         }
+
+        // Target not found
         return false;
     }
 };
