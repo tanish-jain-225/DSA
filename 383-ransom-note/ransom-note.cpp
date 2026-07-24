3327c3c77fad4bd698d9c8f224e39919b0d72f86
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-07-24 12:05:12 +0530

    Time: 1 ms (63.26%) | Memory: 12 MB (29.97%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char, int> need;
-        unordered_map<char, int> have;
+        int n = magazine.size();
+        unordered_map<char, int> f;
 
-        // Count frequency of characters needed
-        for (int i = 0; i < ransomNote.size(); i++) {
-            need[ransomNote[i]]++;
+        for (int i = 0; i < n; i++) {
+            f[magazine[i]]++;
         }
 
-        // Count frequency of characters available
-        for (int i = 0; i < magazine.size(); i++) {
-            have[magazine[i]]++;
-        }
+        int m = ransomNote.size();
 
-        // Compare frequencies
-        for (auto i : need) {
-            char c = i.first;
-            int fneed = i.second;
-            int fhave = have[c];
-
-            if (fhave < fneed) {
+        for (int i = 0; i < m; i++) {
+            if (f.find(ransomNote[i]) == f.end()) {
                 return false;
+            } else {
+                f[ransomNote[i]]--;
+                if (f[ransomNote[i]] == 0) {
+                    f.erase(ransomNote[i]);
+                }
             }
         }
 
