7f6571619bd5bc57f19b99e7d7932fb2fffb7328
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-07-25 16:54:46 +0530

    Time: 46 ms (68.45%) | Memory: 28.9 MB (99.34%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,7 +6,7 @@ public:
 
         vector<vector<int>> ans;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 2; i++) {
 
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
@@ -22,7 +22,7 @@ public:
 
                     while (j < k && nums[j] == nums[j + 1])
                         j++;
-                    while (j < k && nums[k] == nums[k - 1])
+                    while (k > j && nums[k] == nums[k - 1])
                         k--;
 
                     j++;
