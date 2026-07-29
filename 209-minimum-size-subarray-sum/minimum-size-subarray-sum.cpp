c86693db2dc801f5bd53348f7d32bf1c178e79cd
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-07-29 18:04:52 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (30.03%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -2,30 +2,34 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int n = nums.size();
-
         int low = 0;
         int high = 0;
-
         int sum = 0;
         int res = INT_MAX;
 
-        while (high < n) {
-            sum = sum + nums[high];
+        for (high = 0; high < n; high++) {
+            // Pehle Sahi tak badhao : High++
+            sum += nums[high];
 
-            while (sum >= target) {
-                int len = high - low + 1;
-                res = min(res, len);
-                sum = sum - nums[low];
+            while (sum >= target) // Jabtak Sahi store and ghatao : low++
+            {
+                // Store
+                int length = high - low + 1;
+                res = min(res, length);
+
+                // Ghatao
+                sum -= nums[low];
                 low++;
             }
-
-            high++;
+            // Yaha tak pahuch gaye balle balle minimum window hai tumhare pass
         }
 
+        // Agar kuch nahi bane toh return 0
         if (res == INT_MAX) {
             return 0;
         }
 
+        // Bana hai toh answer return
         return res;
     }
 };
