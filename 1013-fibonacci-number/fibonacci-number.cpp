6c9627dddf0150bf973ebf891f2e53bd2be10afd
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-07-30 16:17:11 +0530

    Time: 14 ms (22.57%) | Memory: 7.9 MB (20.70%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n <= 1)
-            return n;
-        return fib(n - 1) + fib(n - 2);
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        int ans1 = fib(n - 1);
+        int ans2 = fib(n - 2);
+
+        return ans1 + ans2;
     }
 };
