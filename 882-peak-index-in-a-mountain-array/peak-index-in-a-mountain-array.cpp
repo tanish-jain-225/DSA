d4645a19e7ddb8f790527e2b33360d1802116add
Tanish Sanghvi <142605718+tanish-jain-225@users.noreply.github.com>
2026-08-01 12:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 63.6 MB (49.18%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int n = arr.size();
-        int ans = -1;
         int low = 0;
         int high = n - 1;
+        int res = -1;
 
         while (low <= high) {
             int guess = (low + high) / 2;
 
             if (arr[guess] < arr[guess + 1]) {
-                ans = guess + 1;
                 low = guess + 1;
             } else {
-                ans = guess;
+                res = guess;
                 high = guess - 1;
             }
         }
 
-        return ans;
+        return res;
     }
 };
