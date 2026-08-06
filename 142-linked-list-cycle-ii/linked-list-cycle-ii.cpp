ee1dfcfadc2071033b2454101d75495dd4fdb132
Tanish Sanghvi <tanishjain020205@gmail.com>
2026-08-06 22:45:31 +0530

    Time: 5 ms (83.18%) | Memory: 11.4 MB (24.58%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -1,31 +1,35 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
-    ListNode* detectCycle(ListNode* head) // Floyd's Tortoise and Hare Algorithm
-    {
+    ListNode* detectCycle(ListNode* head) {
+
+        if (head == NULL || head->next == NULL)
+            return NULL;
+
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while (fast && fast->next) {
+        // Detect cycle
+        while (fast != NULL && fast->next != NULL) {
+
             slow = slow->next;
             fast = fast->next->next;
 
-            if (slow == fast) {
-                slow = head;
-                while (slow != fast) {
-                    slow = slow->next;
-                    fast = fast->next;
-                }
-                return slow;
-            }
+            if (slow == fast)
+                break;
         }
-        return NULL;
+
+        // No cycle
+        if (fast == NULL || fast->next == NULL)
+            return NULL;
+
+        // Reset slow
+        slow = head;
+
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        return slow;
     }
 };
