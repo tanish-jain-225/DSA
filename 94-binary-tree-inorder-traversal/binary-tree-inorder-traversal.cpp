53bf067ff80b7f7672a9166dfcf2ae74f0e119a5
Tanish Sanghvi <tanishjain020205@gmail.com>
2026-08-13 19:14:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (89.58%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -6,52 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    vector<int> inorderTraversal(
-        TreeNode* root) // By Morris Traversal method - TC: O(N) and SC: O(1)
-    {
-        vector<int> ans;
-        TreeNode* curr = root;
+    void helper(TreeNode* root, vector<int>& ans) {
 
-        while (curr) {
-            if (!curr->left) // If there is no left child, visit the current
-                             // node and move to the right child
-            {
-                ans.push_back(curr->val);
-                curr = curr->right;
-            } else {
-                // Find the rightmost node in the left subtree or the temporary
-                // link to the current node
-                TreeNode* prev = curr->left;
-
-                while (prev->right &&
-                       prev->right !=
-                           curr) // Find the rightmost node in the left subtree
-                                 // or the temporary link to the current node
-                {
-                    prev = prev->right;
-                }
-
-                if (!prev->right) // Make a temporary link to the current node
-                {
-                    prev->right = curr;
-                    curr = curr->left;
-                } else // Remove the temporary link and visit the current node
-                {
-                    prev->right = nullptr;
-                    ans.push_back(curr->val);
-                    curr = curr->right;
-                }
-            }
+        // Base Case
+        if (root == nullptr) {
+            return;
         }
 
-        // Return the result vector containing the inorder traversal of the
-        // binary tree
+        // Left
+        helper(root->left, ans);
+
+        // Root
+        ans.push_back(root->val);
+
+        // Right
+        helper(root->right, ans);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+
+        vector<int> ans;
+
+        helper(root, ans);
+
         return ans;
     }
 };
