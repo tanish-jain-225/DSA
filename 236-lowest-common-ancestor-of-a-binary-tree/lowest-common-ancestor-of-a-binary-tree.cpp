b7d9fea6e858f8244b32a94689ff86d9ec9a87c0
Tanish Sanghvi <tanishjain020205@gmail.com>
2026-08-13 21:39:54 +0530

    Time: 59 ms (82.55%) | Memory: 44 MB (27.07%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,36 +7,46 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
 public:
-    // Function to find the lowest common ancestor of two nodes in a binary tree
-    TreeNode*
-    lowestCommonAncestor(TreeNode* root, TreeNode* p,
-                         TreeNode* q) // Time Complexity: O(n), where n is the
-                                      // number of nodes in the binary tree
-    {
-        // Base case: if the root is null, return null
+    int func(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode*& ans) {
+
+        // Base Case
         if (root == nullptr) {
-            return nullptr;
+            return 0;
         }
+
+        // Check Left
+        int leftPart = func(root->left, p, q, ans);
+
+        // Check Right
+        int rightPart = func(root->right, p, q, ans);
+
+        // Check Current Node
+        int selfPart = 0;
 
         if (root == p || root == q) {
-            return root;
+            selfPart = 1;
         }
 
-        // Recur for left and right subtrees
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        // Total number of required nodes found
+        int total = leftPart + rightPart + selfPart;
 
-        // If both left and right are not null, then one key is present in one
-        // subtree and the other key is present in another subtree. So this node
-        // is the lowest common ancestor.
-        if (left && right) {
-            return root;
-        } else if (left) {
-            return left;
-        } else {
-            return right;
+        // First node where both p and q are found
+        if (total == 2 && ans == nullptr) {
+            ans = root;
         }
+
+        return total;
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+
+        TreeNode* ans = nullptr;
+
+        func(root, p, q, ans);
+
+        return ans;
     }
 };
